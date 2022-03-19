640296c9a5c08ecb9ee42b27f209c76232405887
Albina <amasgutova7@gmail.com>
2022-03-19 21:52:47 +0300

    Simple3Test

---
+++ src/test/java/qa/guru/owner/Simple3Test.java
@@ -0,0 +1,11 @@
+package qa.guru.owner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Simple3Test {
+    @Test
+    void assertTrue(){
+        Assertions.assertTrue(3>2);
+    }
+}
